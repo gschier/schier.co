b6ee1bf1118d18a6af13680783ac3a197a7fb1c0
Gregory Schier <gschier1990@gmail.com>
2019-11-14 12:34:57 -0500

    Button colors

--- static/dist/index.js
+++ static/dist/index.js
@@ -12184,7 +12184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62822" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51225" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -12361,4 +12361,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/static/dist/index.js.map
+//# sourceMappingURL=/index.js.map
