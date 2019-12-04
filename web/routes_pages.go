031c72064ce911e11f882541844282d3dafb7208
Gregory Schier <gschier1990@gmail.com>
2019-12-04 14:28:04 -0500

    Show recent posts on home page

--- web/routes_pages.go
+++ web/routes_pages.go
@@ -54,8 +54,26 @@ func routeHome(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	blogPostsOrderBy := prisma.BlogPostOrderByInputDateDesc
+	blogPosts, err := client.BlogPosts(&prisma.BlogPostsParams{
+		Where: &prisma.BlogPostWhereInput{
+			Deleted:   prisma.Bool(false),
+			Published: prisma.Bool(true),
+			DateGt:    prisma.Str("2017-01-01"),
+		},
+		First:   prisma.Int32(5),
+		OrderBy: &blogPostsOrderBy,
+	},
+	).Exec(r.Context())
+	if err != nil {
+		log.Println("Failed to fetch blog posts: " + err.Error())
+		http.Error(w, "Failed to fetch blog posts", http.StatusInternalServerError)
+		return
+	}
+
 	renderTemplate(w, r, homeTemplate(), &pongo2.Context{
 		"favoriteThings": favoriteThings,
 		"projects":       projects,
+		"blogPosts":      blogPosts,
 	})
 }
