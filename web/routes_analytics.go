60b6ffd9bb78adc5d936e76f3245ff846bd367de
Gregory Schier <gschier1990@gmail.com>
2019-12-24 11:01:22 -0500

    Analytics page

--- web/routes_analytics.go
+++ web/routes_analytics.go
@@ -2,15 +2,55 @@ package web
 
 import (
 	"context"
+	"github.com/flosch/pongo2"
 	"github.com/gorilla/mux"
 	"github.com/gschier/schier.dev/generated/prisma-client"
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func AnalyticsRoutes(router *mux.Router) {
 	router.HandleFunc("/t", routeTrack).Methods(http.MethodGet)
+	router.HandleFunc("/analytics", Admin(routeAnalytics)).Methods(http.MethodGet)
+}
+
+var analyticsTemplate = pageTemplate("analytics/analytics.html")
+
+func routeAnalytics(w http.ResponseWriter, r *http.Request) {
+	client := ctxPrismaClient(r)
+
+	now := time.Now()
+	sevenDaysAgo := now.Add(-time.Hour * 24 * 7).Format(time.RFC3339)
+	orderBy := prisma.AnalyticsPageViewOrderByInputTimeDesc
+
+	views, err := client.AnalyticsPageViews(&prisma.AnalyticsPageViewsParams{
+		Where: &prisma.AnalyticsPageViewWhereInput{
+			TimeGte: &sevenDaysAgo,
+		},
+		OrderBy: &orderBy,
+	}).Exec(r.Context())
+	if err != nil {
+		http.Error(w, "Failed to query analytics", http.StatusInternalServerError)
+		return
+	}
+
+	stats := make(map[int]int)
+	for _, view := range views {
+		t, _ := time.Parse(time.RFC3339, view.Time)
+		day := int(now.Sub(t).Hours() / 24)
+		stats[day] += 1
+	}
+
+	statsList := make([]int, 0)
+	for i := 6; i >= 0; i-- {
+		statsList = append(statsList, stats[i])
+	}
+
+	renderTemplate(w, r, analyticsTemplate(), &pongo2.Context{
+		"stats": statsList,
+	})
 }
 
 func routeTrack(w http.ResponseWriter, r *http.Request) {
