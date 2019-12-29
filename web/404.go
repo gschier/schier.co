aef400ab0f3b5a6e572544f86859d28c678f545e
Gregory Schier <gschier1990@gmail.com>
2019-12-29 08:11:32 -0500

    Blog posts on 404

--- web/404.go
+++ web/404.go
@@ -1,7 +1,10 @@
 package web
 
 import (
+	"github.com/flosch/pongo2"
 	"github.com/gorilla/mux"
+	schier "github.com/gschier/schier.dev"
+	"log"
 	"net/http"
 )
 
@@ -12,10 +15,22 @@ func NotFoundRoutes(router *mux.Router) {
 var notFoundTemplate = pageTemplate("404.html")
 
 func routeNotFound(w http.ResponseWriter, r *http.Request) {
+	// Can't get from request context because middleware didn't run
+	client := schier.NewPrismaClient()
+
+	blogPosts, err := client.BlogPosts(RecentBlogPosts(6, nil)).Exec(r.Context())
+	if err != nil {
+		log.Println("Failed to fetch blog posts: " + err.Error())
+		http.Error(w, "Failed to fetch blog posts", http.StatusInternalServerError)
+		return
+	}
+
 	// A Content-Type header has to be set before calling w.WriteHeader,
 	// otherwise WriteHeader is called twice (from this handler and
 	// the compression handler) and the response breaks.
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusNotFound)
-	renderTemplate(w, r, notFoundTemplate(), nil)
+	renderTemplate(w, r, notFoundTemplate(), &pongo2.Context{
+		"blogPosts": blogPosts,
+	})
 }
