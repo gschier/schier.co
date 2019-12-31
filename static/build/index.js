da058d39359d326ba4c88398d4f0d8e204924f94
Gregory Schier <gschier1990@gmail.com>
2019-12-31 17:28:29 -0500

    Typo

--- static/build/index.js
+++ static/build/index.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62888" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62962" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
