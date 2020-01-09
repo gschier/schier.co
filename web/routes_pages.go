eedae9c6d4697e4a373b5f1721f57a8d0b55c81c
Gregory Schier <gschier1990@gmail.com>
2020-01-08 21:55:01 -0500

    More home page posts

--- web/routes_pages.go
+++ web/routes_pages.go
@@ -67,7 +67,7 @@ func routeProjects(w http.ResponseWriter, r *http.Request) {
 
 func routeHome(w http.ResponseWriter, r *http.Request) {
 	client := ctxPrismaClient(r)
-	blogPosts, err := client.BlogPosts(RecommendedBlogPosts(8, nil)).Exec(r.Context())
+	blogPosts, err := client.BlogPosts(RecommendedBlogPosts(10, nil)).Exec(r.Context())
 	if err != nil {
 		log.Println("Failed to fetch blog posts: " + err.Error())
 		http.Error(w, "Failed to fetch blog posts", http.StatusInternalServerError)
