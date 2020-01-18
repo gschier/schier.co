fecaad107017a2e2e7812db2d703945b3de0d941
Gregory Schier <gschier1990@gmail.com>
2020-01-18 13:19:09 -0500

    Some minor tweaks

--- go.mod
+++ go.mod
@@ -15,20 +15,16 @@ require (
 	github.com/gorilla/mux v1.7.3
 	github.com/gosimple/slug v1.9.0
 	github.com/machinebox/graphql v0.2.2
-	github.com/mailgun/mailgun-go/v3 v3.6.3 // indirect
 	github.com/mailjet/mailjet-apiv3-go v0.0.0-20190724151621-55e56f74078c
 	github.com/mileusna/useragent v0.0.0-20190129205925-3e331f0949a5
 	github.com/minio/minio-go v6.0.14+incompatible
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/oschwald/maxminddb-golang v1.6.0
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/prisma/prisma-client-lib-go v0.0.0-20181017161110-68a1f9908416
-	github.com/russross/blackfriday v2.0.0+incompatible // indirect
 	github.com/russross/blackfriday/v2 v2.0.1
-	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
-	github.com/ua-parser/uap-go v0.0.0-20190826212731-daf92ba38329 // indirect
 	github.com/writeas/go-strip-markdown v2.0.1+incompatible
 	golang.org/x/crypto v0.0.0-20191111213947-16651526fdb4
-	gopkg.in/yaml.v2 v2.2.2
 )
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,51 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
+    require('postcss-preset-env')({ stage: 0 }),
     require('autoprefixer'),
+    require('tailwindcss')({
+      important: true,
+      theme: {
+        colors: {
+          primary: { ...colors.blue, '50': '#f5fbff' },
+          background: colors.white,
+          foreground: colors.gray['800'],
+          danger: colors.red,
+          warning: colors.orange,
+          notice: colors.yellow,
+          info: colors.blue,
+          gray: colors.gray,
+          transparent: 'transparent',
+        },
+        textColor: {
+          gray: colors.gray,
+          inverted: colors.gray['200'],
+          primary: colors.gray['700'],
+          secondary: colors.blue['600'],
+          notice: colors.yellow['700'],
+          warning: colors.orange['600'],
+          danger: colors.red['600'],
+          info: colors.blue['600'],
+          syntax1: colors.purple['600'],
+          syntax2: colors.blue['600'],
+          syntax3: colors.indigo['600'],
+          syntax4: colors.gray['600'],
+        },
+        opacity: {
+          '0': '0',
+          '10': '.1',
+          '20': '.2',
+          '30': '.3',
+          '40': '.4',
+          '50': '.5',
+          '60': '.6',
+          '70': '.7',
+          '80': '.8',
+          '90': '.9',
+          '100': '1',
+        },
+      },
+    }),
   ],
 };
