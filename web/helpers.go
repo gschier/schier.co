e2e23ac4ad2acead5e6d7ef20931e8c836e9b4aa
Gregory Schier <gschier1990@gmail.com>
2020-04-30 20:32:34 -0700

    De-prioritize older posts

--- web/helpers.go
+++ web/helpers.go
@@ -165,7 +165,7 @@ func CalculateScore(age time.Duration, votes, views, words int32) int32 {
 		return 999999
 	}
 
-	score := float64(votes*400+views) / (math.Min(days, 70) + 1)
+	score := float64(votes*400+views) / (days + 1)
 
 	if words < 200 {
 		score /= 2
