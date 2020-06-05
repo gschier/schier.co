09697b9f010b61411a1caea76dee7e64b340d84d
Gregory Schier <gschier1990@gmail.com>
2020-06-05 09:32:06 -0700

    Auto theme selection

--- frontend/index.all.js
+++ frontend/index.all.js
@@ -5,8 +5,15 @@ const htmlEl = document.querySelector('html');
 document.querySelectorAll('[data-toggle-theme]').forEach((el) => {
   el.addEventListener('click', e => {
     e.preventDefault();
-    const theme = htmlEl.getAttribute('theme') || '';
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    let theme = htmlEl.getAttribute('theme') || 'auto';
+    let newTheme = theme;
+
+    if (e.metaKey || e.ctrlKey) {
+      newTheme = '';
+    } else {
+      newTheme = theme.includes('dark') ? 'light' : 'dark';
+    }
+
     localStorage.setItem('theme', newTheme);
     htmlEl.setAttribute('theme', newTheme);
     showIcon();
@@ -14,17 +21,18 @@ document.querySelectorAll('[data-toggle-theme]').forEach((el) => {
 });
 
 function showIcon() {
-  const theme = htmlEl.getAttribute('theme') || '';
+  const theme = htmlEl.getAttribute('theme') || 'light-auto';
+
   document.querySelectorAll('[data-toggle-theme]').forEach(el => {
+    const iconAuto = el.querySelector('[data-theme="auto"]');
     const iconLight = el.querySelector('[data-theme="light"]');
     const iconDark = el.querySelector('[data-theme="dark"]');
-    if (theme === 'dark') {
-      iconLight.style.display = 'none';
-      iconDark.style.display = 'inline-block';
-    } else {
-      iconLight.style.display = 'inline-block';
-      iconDark.style.display = 'none';
-    }
+
+    iconAuto.style.display = theme.includes('-auto') ? 'inline-block' : 'none';
+    iconLight.style.display = theme === 'light' ? 'inline-block' : 'none';
+    iconDark.style.display = theme === 'dark' ? 'inline-block' : 'none';
+
+    console.log('Using theme', theme);
   });
 }
 
--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,97 +1,11 @@
-const { colors } = require('tailwindcss/defaultTheme');
-
 module.exports = {
   plugins: [
     require('autoprefixer'),
+    require('postcss-nested'),
+    require('postcss-import'),
     require('tailwindcss')({
       important: true,
-      theme: {
-        colors: {
-          background: 'var(--bg)',
-          btn: {
-            'default': 'var(--bg-btn)',
-            'hover': 'var(--bg-btn-hover)',
-          },
-          primary: {
-            '050': 'var(--color-primary-050)',
-            '100': 'var(--color-primary-100)',
-            '200': 'var(--color-primary-200)',
-            '300': 'var(--color-primary-300)',
-            '400': 'var(--color-primary-400)',
-            '500': 'var(--color-primary-500)',
-          },
-          danger: {
-            '100': 'var(--color-danger-100)',
-            '400': 'var(--color-danger-400)',
-            '500': 'var(--color-danger-500)',
-          },
-          warning: {
-            '100': 'var(--color-warning-100)',
-            '400': 'var(--color-warning-400)',
-            '500': 'var(--color-warning-500)',
-          },
-          notice: {
-            '100': 'var(--color-notice-100)',
-            '400': 'var(--color-notice-400)',
-            '500': 'var(--color-notice-500)',
-          },
-          info: {
-            '100': 'var(--color-info-100)',
-            '400': 'var(--color-info-400)',
-            '500': 'var(--color-info-500)',
-          },
-          gray: {
-            '900': 'var(--color-gray-900)',
-            '800': 'var(--color-gray-800)',
-            '700': 'var(--color-gray-700)',
-            '600': 'var(--color-gray-600)',
-            '500': 'var(--color-gray-500)',
-            '400': 'var(--color-gray-400)',
-            '300': 'var(--color-gray-300)',
-            '200': 'var(--color-gray-200)',
-            '100': 'var(--color-gray-100)',
-          },
-        },
-        textColor: {
-          btn: {
-            'default': 'var(--text-btn)',
-            'hover': 'var(--text-btn-hover)',
-          },
-          gray: {
-            '900': 'var(--color-gray-900)',
-            '800': 'var(--color-gray-800)',
-            '700': 'var(--color-gray-700)',
-            '600': 'var(--color-gray-600)',
-            '500': 'var(--color-gray-500)',
-            '400': 'var(--color-gray-400)',
-            '300': 'var(--color-gray-300)',
-            '200': 'var(--color-gray-200)',
-            '100': 'var(--color-gray-100)',
-          },
-          link: 'var(--text-link)',
-          notice: 'var(--text-notice)',
-          warning: 'var(--text-warning)',
-          danger: 'var(--text-danger)',
-          info: 'var(--text-info)',
-          syntax1: 'var(--text-syntax-1)',
-          syntax2: 'var(--text-syntax-2)',
-          syntax3: 'var(--text-syntax-3)',
-          syntax4: 'var(--text-syntax-4)',
-        },
-        opacity: {
-          '0': '0',
-          '10': '.1',
-          '20': '.2',
-          '30': '.3',
-          '40': '.4',
-          '50': '.5',
-          '60': '.6',
-          '70': '.7',
-          '80': '.8',
-          '90': '.9',
-          '100': '1',
-        },
-      },
+      theme: require('./tailwind/theme'),
     }),
   ],
 };
