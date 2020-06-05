10eee7a64076e9f9c138ffaa0d50da5671f0c3d1
Gregory Schier <gschier1990@gmail.com>
2020-06-04 19:06:42 -0700

    Dark mode

--- frontend/index.all.js
+++ frontend/index.all.js
@@ -1,1 +1,11 @@
 import './styles/styles.css';
+
+for (const el of document.querySelectorAll('[data-toggle-theme]')) {
+  el.addEventListener('click', e => {
+    e.preventDefault();
+    const theme = document.querySelector('html').getAttribute('theme') || '';
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    document.querySelector('html').setAttribute('theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  })
+}
