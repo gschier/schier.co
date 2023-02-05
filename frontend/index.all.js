a43376e23997c8592a5bac9a798139bee8ce1c17
Gregory Schier <gschier1990@gmail.com>
2023-02-05 11:36:52 -0800

    Fade theme changes

--- frontend/index.all.js
+++ frontend/index.all.js
@@ -1,1 +1,10 @@
 import './styles/styles.css';
+
+// Inject CSS to enable transitions after a delay. The delay
+// is to prevent the transitions from happening on page load.
+setTimeout(() => {
+    const css = '*{transition:all 150ms ease-in-out}';
+    const style = document.createElement('style');
+    style.innerHTML = css;
+    document.querySelector('head').appendChild(style);
+}, 1000);
