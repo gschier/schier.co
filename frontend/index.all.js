bb6ca270fc93248974fd9aa2ef43988221ad96de
Gregory Schier <gschier1990@gmail.com>
2023-02-06 14:52:25 -0800

    Fix theme transition

--- frontend/index.all.js
+++ frontend/index.all.js
@@ -3,7 +3,7 @@ import './styles/styles.css';
 // Inject CSS to enable transitions after a delay. The delay
 // is to prevent the transitions from happening on page load.
 setTimeout(() => {
-    const css = '*{transition:all 150ms ease-in-out}';
+    const css = 'html,button{transition:all 250ms}';
     const style = document.createElement('style');
     style.innerHTML = css;
     document.querySelector('head').appendChild(style);
